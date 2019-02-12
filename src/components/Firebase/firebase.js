f103aab27e163e8f14626e46cab2d9327f34b98d
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-02-11 19:59:33 -0600

    Firebase's Authentication API: Sign Up with React and Firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,5 @@
 import app from "firebase/app";
-
+import 'firebase/auth';
 /**
  * We use a .env file to declare our firebase credentials and keep them hidden
  */
@@ -27,7 +27,23 @@ const config =
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    /* Auth API */
+    doCreateUserWithEmailAndPassword = (email, password) => 
+      this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) => 
+      this.auth.signInWithEmailAndPassword(email, password);
+    
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => 
+      this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
